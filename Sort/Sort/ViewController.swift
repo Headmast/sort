a5b62f4c9d3b362f94e6146f54b88419e101e9a6
Kirill Klebanov <headmast@bk.ru>
2021-01-01 22:01:22 +0300

    Create base Xcode project

---
+++ Sort/Sort/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sort
+//
+//  Created by Kirill Klebanov on 1/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
